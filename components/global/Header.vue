185c99a780639f0d64e55a7603bfa28b7367872a
thejandae <tjandae@gmail.com>
2021-08-07 01:37:33 +0800

    add adsense

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -14,6 +14,8 @@
   </nav>
 </template>
 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3925356940316949"
+     crossorigin="anonymous"></script>
 <script>
 export default {
   name: 'Header',

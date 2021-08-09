5782b8d5a1c3d3cd8d4429759c192b250cc8edd1
thejandae <tjandae@gmail.com>
2021-08-10 05:23:40 +0800

    add link icon

--- pages/links/_link.vue
+++ pages/links/_link.vue
@@ -6,15 +6,15 @@
       </nav>
 
       <article>
-        <h5
+        <!-- <h5
           v-if="post.createdAt"
           class="inline-block py-1 px-2 my-2 bg-gray text-white text-sm font-medium rounded-sm whitespace-no-wrap"
         >
           {{ formatDate(post.createdAt) }}
-        </h5>
-        <h1 class="">{{ post.title }}</h1>
-        <p class="mt-1 mb-4 text-primary-600 dark:text-primary-400">{{ post.description }}</p>
-        <nuxt-content :document="post" />
+        </h5> -->
+        <h1 class="">{{ post.description }}</h1>
+        <p class="mt-1 mb-4 text-primary-600 dark:text-primary-400">{{ post.title }}</p>
+        <nuxt-content :document="post" class="article-content" />
       </article>
     </section>
   </main>
@@ -39,3 +39,22 @@ export default {
   },
 }
 </script>
+
+<style  lang="scss">
+.article-content {
+  a:after {
+    content: url("data:image/svg+xml;charset=UTF-8, <svg aria-hidden= 'true' focusable= 'false' data-prefix= 'fas' data-icon= 'external-link-square-alt' class='svg-inline--fa fa-external-link-square-alt fa-w-14' role= 'img' xmlns= 'http://www.w3.org/2000/svg' viewBox='0 0 448 512' ><path fill= 'white' d='M448 80v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48zm-88 16H248.029c-21.313 0-32.08 25.861-16.971 40.971l31.984 31.987L67.515 364.485c-4.686 4.686-4.686 12.284 0 16.971l31.029 31.029c4.687 4.686 12.285 4.686 16.971 0l195.526-195.526 31.988 31.991C358.058 263.977 384 253.425 384 231.979V120c0-13.255-10.745-24-24-24z'></path></svg>");
+    width: 15px;
+    height: 17px;
+    display: inline-block;
+    margin-left: 5px;
+    opacity: 0.3;
+  }
+
+  h2 {
+    a:after {
+      content: '';
+    }
+  }
+}
+</style>

5814091b63a27b303a79a7320206ab54a46d3132
jandae <tjandae@gmail.com>
2022-02-26 16:32:20 +0800

    update  link locations

--- components/socials/socials.vue
+++ components/socials/socials.vue
@@ -1,21 +1,8 @@
 <template>
-  <div class="inline-grid grid-cols-2 gap-4 mt-4 mx-auto">
-    <a
-      class="btn px-1 items-center justify-center flex flex-col"
-      href="https://openinapp.co/youtube/thejandae"
-      target="_blank"
-      >Tech Youtube
-
-      <script src="https://apis.google.com/js/platform.js"></script>
-
-      <div
-        class="g-ytsubscribe"
-        data-channelid="UCS4bNn83QPhGftBoGmKf7ew"
-        data-layout="default"
-        data-theme="dark"
-        data-count="default"
-      ></div>
-    </a>
+  <div class="inline-grid grid-cols-2 gap-4 mt-4 mx-auto">   
+    <a class="btn px-1 items-center justify-center flex" href="https://openinapp.co/url/87a05" target="_blank"
+      >Gaming Twitch</a
+    >
     <a
       class="btn px-1 items-center justify-center flex flex-col"
       href="https://openinapp.co/youtube/thejandae-build-log"
@@ -30,20 +17,15 @@
         data-count="default"
       ></div>
     </a>
-    <a class="btn px-1 items-center justify-center flex" href="https://openinapp.co/url/95fe2" target="_blank"
-      >Twitch</a
-    >
-    <a class="btn px-1 items-center justify-center flex" href="https://openinapp.co/url/87a05" target="_blank"
-      >Gaming Twitch</a
-    >
-    <a class="btn px-1 items-center justify-center flex" href="https://openinapp.co/facebook/a9608" target="_blank"
-      >Facebook</a
-    >
-
     <a class="btn px-1 items-center justify-center flex" href="https://openinapp.co/facebook/41f0c" target="_blank"
       >Facebook Gaming</a
     >
-
+    <a class="btn px-1 items-center justify-center flex" href="https://openinapp.co/url/95fe2" target="_blank"
+      >Twitch</a
+    >    
+    <a class="btn px-1 items-center justify-center flex" href="https://openinapp.co/facebook/a9608" target="_blank"
+      >Facebook</a
+    >
     <a
       class="btn btn-ribbon px-1 items-center justify-center inline-block align-middle flex flex-col relative"
       href="https://github.com/jandae"
@@ -58,6 +40,22 @@
       target="_blank"
     >
       <span class="justify-center align-middle">Tiktok</span>
+    </a>
+    <a
+      class="btn px-1 items-center justify-center flex flex-col"
+      href="https://openinapp.co/youtube/thejandae"
+      target="_blank"
+      >Personal Youtube
+
+      <script src="https://apis.google.com/js/platform.js"></script>
+
+      <div
+        class="g-ytsubscribe"
+        data-channelid="UCS4bNn83QPhGftBoGmKf7ew"
+        data-layout="default"
+        data-theme="dark"
+        data-count="default"
+      ></div>
     </a>
   </div>
 </template>
